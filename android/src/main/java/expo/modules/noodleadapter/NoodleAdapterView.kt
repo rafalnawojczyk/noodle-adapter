2a5f8161825e970da16dcbc7d9945c2bcb636f06
rafalnawojczyk <rafal.nawojczyk@riotters.com>
2024-06-14 21:24:57 +0200

    cleanup: remove unnecessary files

--- android/src/main/java/expo/modules/noodleadapter/NoodleAdapterView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.noodleadapter
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class NoodleAdapterView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
